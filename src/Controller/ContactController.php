9bfbbc0b758dd9beb9aa7315765925f2c03348f9
jadeg <jadegrau@epsi.fr>
2021-12-17 22:22:07 +0100

    Initial Commit

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Contact;
+use App\Form\ContactType;
+use App\Repository\ContactRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+
+    /**
+     * @var ContactRepository
+     */
+    private $contactRepository;
+
+    public function __construct(ContactRepository $contactRepository){
+        $this->contactRepository = $contactRepository;
+    }
+
+    /**
+     * @Route("/", name="contact")
+     */
+    public function index(Request $request): Response
+    {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($form->getData());
+            $entityManager->flush();
+
+            dump("Envoyé en DB");
+        }
+
+
+        return $this->renderForm('contact/index.html.twig', [
+            'contacts' => $this->contactRepository->findAll(),
+            'form' => $form,
+
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="contactId")
+     */
+    public function login(Request $request, string $id): Response
+    {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+
+
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($form->getData());
+            $entityManager->flush();
+
+            dump("Envoyé en DB");
+        }
+
+
+        return $this->renderForm('contact/index.html.twig', [
+            'contacts' => $this->contactRepository->findAll(),
+            'mycontact' => $this->contactRepository->find($id),
+            'form' => $form,
+        ]);
+    }
+}
